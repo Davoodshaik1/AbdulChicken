aed5c30ee1f35f69840b9978fd1c9e7210597d56
SHAIK DAVOOD UMAR <123303250+Davoodshaik1@users.noreply.github.com>
2025-05-15 22:50:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ transporter.verify((error, success) => {
 });
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/abduls-chicken', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
