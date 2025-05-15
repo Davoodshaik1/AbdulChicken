a1bbe1a60162ea86ee9c1da578236955757e2ffa
SHAIK DAVOOD UMAR <123303250+Davoodshaik1@users.noreply.github.com>
2025-05-15 21:53:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://chicken-mutton-shop.vercel.app/',
   methods: ['GET', 'POST'],
   credentials: true,
 }));
